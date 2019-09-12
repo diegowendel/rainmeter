6949ff6c0d17e7e1029ba3575d4acaddc1685192
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-12 21:50:19 +0000

    Deploy diegowendel/rainmeter to github.com/diegowendel/rainmeter.git:gh-pages

---
+++ precache-manifest.f712f47c51cc730b3f1307f50a6ba3ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b8861f726d31dbd76be0c796d4e074",
+    "url": "/rainmeter/index.html"
+  },
+  {
+    "revision": "59ed9e3fa7705640f560",
+    "url": "/rainmeter/static/css/main.9a937666.chunk.css"
+  },
+  {
+    "revision": "3f435f59edf41cb17c0c",
+    "url": "/rainmeter/static/js/2.6d436d7d.chunk.js"
+  },
+  {
+    "revision": "59ed9e3fa7705640f560",
+    "url": "/rainmeter/static/js/main.1739ccfa.chunk.js"
+  },
+  {
+    "revision": "468598b06fb139560c91",
+    "url": "/rainmeter/static/js/runtime~main.74d6ad6b.js"
+  }
+]);
