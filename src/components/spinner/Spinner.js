efacf371ba77d40a971eeb5dcf9fa571dcdf7dcf
diegowendel <dgnnsx@gmail.com>
2019-09-12 15:10:30 -0300

    Fix structure
    Change-Id: Ib28b41e94607467aeec646390b560e1ca4148669

--- src/components/spinner/Spinner.js
+++ src/components/spinner/Spinner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons/index';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import '../../styles/Spinner.scss';
 
 function Spinner() {
